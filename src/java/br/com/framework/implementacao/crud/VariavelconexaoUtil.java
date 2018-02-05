2dde1d086633752dbc6574071e57059ce0f11e89
Michael Douglas <michaeldouglas.10.94@gmail.com>
2018-02-04 23:49:55 -0200

    first commit

---
+++ src/java/br/com/framework/implementacao/crud/VariavelconexaoUtil.java
@@ -0,0 +1,9 @@
+package br.com.framework.implementacao.crud;
+
+/*nome do caminho do JNDI datasource Tomcat
+@author  Michael
+ */
+public class VariavelconexaoUtil {
+    
+    public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+}
